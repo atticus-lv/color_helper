9cd4d3b5949a1c0b45da96f71db10010e5897f09
atticus-lv <1029910278@qq.com>
2022-01-25 04:15:17 +0800

    use colorsys instead

--- ops/op_palette_manage.py
+++ ops/op_palette_manage.py
@@ -2,49 +2,6 @@ import bpy
 from bpy.props import *
 
 
-def rgb_to_hsv(r, g, b):
-    maxc = max(r, g, b)
-    minc = min(r, g, b)
-    v = maxc
-    if minc == maxc:
-        return 0.0, 0.0, v
-    s = (maxc - minc) / maxc
-    rc = (maxc - r) / (maxc - minc)
-    gc = (maxc - g) / (maxc - minc)
-    bc = (maxc - b) / (maxc - minc)
-    if r == maxc:
-        h = bc - gc
-    elif g == maxc:
-        h = 2.0 + rc - bc
-    else:
-        h = 4.0 + gc - rc
-    h = (h / 6.0) % 1.0
-    return h, s, v
-
-
-def hsv_to_rgb(h, s, v):
-    if s == 0.0:
-        return v, v, v
-    i = int(h * 6.0)  # XXX assume int() truncates!
-    f = (h * 6.0) - i
-    p = v * (1.0 - s)
-    q = v * (1.0 - s * f)
-    t = v * (1.0 - s * (1.0 - f))
-    i = i % 6
-    if i == 0:
-        return v, t, p
-    if i == 1:
-        return q, v, p
-    if i == 2:
-        return p, v, t
-    if i == 3:
-        return p, q, v
-    if i == 4:
-        return t, p, v
-    if i == 5:
-        return v, p, q
-
-
 def redraw_area():
     for window in bpy.context.window_manager.windows:
         for area in window.screen.areas:
@@ -251,45 +208,7 @@ class CH_OT_remove_color(bpy.types.Operator):
         return {'FINISHED'}
 
 
-class CH_OT_sort_color(bpy.types.Operator):
-    bl_idname = 'ch.sort_color'
-    bl_label = 'Sort Color'
 
-    mode: EnumProperty(name='Mode', items=[
-        ('0', 'Hue', ''),
-        ('1', 'Saturation', ''),
-        ('2', 'Value', ''),
-    ])
-
-    reverse: BoolProperty(name='Reverse', default=False)
-
-    palette_index: IntProperty()
-
-    def execute(self, context):
-        collection = context.scene.ch_palette_collection[context.scene.ch_palette_collection_index]
-        palette = collection.palettes[self.palette_index]
-
-        original_colors = sorted([
-            (
-                [(r, g, b)[int(self.mode)] for (r, g, b) in [rgb_to_hsv(*color.color[:3])]],  # sort key
-                tuple(color.color)  # source color
-            ) for color in palette.colors], reverse=self.reverse
-        )
-
-        for new_index, (hsv, color) in enumerate(original_colors):
-            palette.colors[new_index].color = color
-
-        redraw_area()
-
-        return {'FINISHED'}
-
-    def draw(self, context):
-        layout = self.layout
-        layout.prop(self, 'mode', expand=True)
-        layout.prop(self, 'reverse')
-
-    def invoke(self, context, event):
-        return context.window_manager.invoke_props_dialog(self, width=200)
 
 
 classes = (
@@ -302,7 +221,6 @@ classes = (
     CH_OT_copy_palette,
     CH_PT_move_palette,
 
-    CH_OT_sort_color,
 
     CH_OT_add_color,
     CH_OT_remove_color
